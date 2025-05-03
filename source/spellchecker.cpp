fb125ea5690ce2aa24665badc7595141fd632e51
vovatrykoz <vovatrykoz@gmail.com>
2025-05-03 13:57:19 +0200

    use alloca in the windows version for stack allocated arrays

--- source/spellchecker.cpp
+++ source/spellchecker.cpp
@@ -4,6 +4,8 @@
 #include <cstring>
 #include <vector>
 
+#ifdef __GNUC__
+
 int lev(const std::string& a, const std::string& b) {
     const std::size_t a_size = a.size();
     const std::size_t b_size = b.size();
@@ -33,6 +35,47 @@ int lev(const std::string& a, const std::string& b) {
 
     return mat[a_size][b_size];
 }
+
+#else
+
+#include <alloca.h>
+
+// MSVS does not support variable length arrays
+// we have to use _alloca to manually allocate the memory on the stack instead
+int lev(const std::string& a, const std::string& b) {
+    const std::size_t a_size = a.size();
+    const std::size_t b_size = b.size();
+    const int rows = a_size + 1;
+    const int cols = b_size + 1;
+    const int matrixSize = rows * cols * sizeof(int);
+
+    int* mat = (int*)alloca(matrixSize);
+    std::memset(mat, 0, matrixSize);
+
+    for (int row = 1; row <= a_size; row++) {
+        mat[row * cols] = row;
+    }
+
+    for (int col = 1; col <= b_size; col++) {
+        mat[col] = col;
+    }
+
+    for (std::size_t row = 1; row <= a_size; row++) {
+        for (std::size_t col = 1; col <= b_size; col++) {
+            const int addition = a[row - 1] == b[col - 1] ? 0 : 1;
+
+            const int left = mat[row * cols + (col - 1)] + 1;
+            const int up = mat[(row - 1) * cols + col] + 1;
+            const int diag = mat[(row - 1) * cols + (col - 1)] + addition;
+
+            mat[row * cols + col] = std::min(left, std::min(up, diag));
+        }
+    }
+
+    return mat[a_size * cols + b_size];
+}
+
+#endif
 
 std::unordered_map<std::string, int> baseListAroundWord(
     const std::string& input, const std::vector<std::string>& words) {
